425c36257c5c9831acf567e32c97a2fc8aa5426e
Kacper <kxkrzyzkowski@gmail.com>
2023-05-04 21:57:15 +0200

    lab7

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Test1 from "./components/1-componentClass";
+import Test2 from "./components/2-componentFunctional";
+import QuizComponent from "./components/QuizComponent";
+
+function App() {
+  return (
+    <div>
+      <QuizComponent/>
+    </div>
+  );
+}
+
+export default App;
